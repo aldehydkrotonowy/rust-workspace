706e4287d53b73a734dd380ed39d462018dae3da
Norbet Gajda <norbert.gajda@tlen.pl>
2021-09-28 21:26:18 +0200

    closures

--- workspace/basic_playground/src/main.rs
+++ workspace/basic_playground/src/main.rs
@@ -1,7 +1,11 @@
+pub mod closures;
 pub mod find_and_show;
 pub mod slices;
 
 #[warn(unused_variables)]
 fn main() {
-    println!("hello world")
+    find_and_show::find_and_show();
+    closures::run();
+    // slices::slices_play();
+    let _a: String = String::from("akapulko");
 }
