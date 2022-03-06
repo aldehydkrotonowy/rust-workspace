ba631b0997441ee67d01b49291299348b8b4f53f
norbert gajda <norbert.gajda@gmail.com>
2022-03-06 09:38:09 +0100

    rocket web server beginning

--- workspace/readme.md
+++ workspace/readme.md
@@ -1,3 +1,7 @@
 1. add new workspace
    cargo new some_name [--lib]
 2. edit cargo.toml
+
+A. watch and rebuild in workspace
+cargo watch -x "run --bin rocket_web_server"
+cargo watch -x "run -p rocket_web_server"
--- workspace/witter_backend/src/main.rs
+++ workspace/witter_backend/src/main.rs
@@ -1,54 +1,50 @@
-// use dotenv;
+use dotenv;
 
-// use sqlx::query;
-// use sqlx::PgPool;
-// use sqlx::Pool;
-// use tide::Request;
-// use tide::Server; //friendly HTTP server
+use sqlx::query;
+use sqlx::PgPool;
+use sqlx::Pool;
+use tide::Request;
+use tide::Server; //friendly HTTP server
 
-// #[async_std::main]
-// async fn main() -> Result<(), Error> {
-// 	dotenv::dotenv().ok();
-// 	pretty_env_logger::init();
+#[async_std::main]
+async fn main() -> Result<(), Error> {
+	dotenv::dotenv().ok();
+	pretty_env_logger::init();
 
-// 	let db_url = std::env::var("DATABASE_URL")?;
-// 	// dbg!(db_url);
+	let db_url = std::env::var("DATABASE_URL")?;
+	// dbg!(db_url);
 
-// 	let db_pool: PgPool = Pool::new(&db_url).await?;
-// 	let rows = query!("select 1 as one").fetch_one(&db_pool).await?;
-// 	dbg!(rows);
-// 	let my_name = "ngajda".to_string();
-// 	let state = State {
-// 		db_pool,
-// 		my_name
-// 	};
-// 	let mut app: Server<State> = Server::with_state(state);
-// 	app.at("/").get(|req: Request<State>| async move {
-// 		let db_pool: &PgPool = &req.state().db_pool;
+	let db_pool: PgPool = Pool::new(&db_url).await?;
+	let rows = query!("select 1 as one").fetch_one(&db_pool).await?;
+	dbg!(rows);
+	let my_name = "ngajda".to_string();
+	let state = State { db_pool, my_name };
+	let mut app: Server<State> = Server::with_state(state);
+	app.at("/").get(|req: Request<State>| async move {
+		let db_pool: &PgPool = &req.state().db_pool;
 
-// 		query!("select 1 as one").fetch_one(db_pool).await?;
-		
-// 		Ok(format!("Hello, world. {}!", &req.state().my_name))
-// 	});
+		query!("select 1 as one").fetch_one(db_pool).await?;
+		Ok(format!("Hello, world. {}!", &req.state().my_name))
+	});
 
-// 	app.listen("127.0.0.1:9000").await?;
+	app.listen("127.0.0.1:9000").await?;
 
-// 	Ok(())
-// }
+	Ok(())
+}
 
-// #[derive(Debug, Clone)]
-// struct State {
-// 	db_pool: PgPool,
-// 	my_name: String,
-// }
+#[derive(Debug, Clone)]
+struct State {
+	db_pool: PgPool,
+	my_name: String,
+}
 
-// #[derive(thiserror::Error, Debug)]
-// enum Error {
-// 	#[error(transparent)]
-// 	DbError(#[from] sqlx::Error),
+#[derive(thiserror::Error, Debug)]
+enum Error {
+	#[error(transparent)]
+	DbError(#[from] sqlx::Error),
 
-// 	#[error(transparent)]
-// 	IoError(#[from] std::io::Error),
-// 	#[error(transparent)]
-// 	VarError(#[from] std::env::VarError),
-// }
+	#[error(transparent)]
+	IoError(#[from] std::io::Error),
+	#[error(transparent)]
+	VarError(#[from] std::env::VarError),
+}
